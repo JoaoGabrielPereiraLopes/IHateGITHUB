ccb81e395e4bc97bae4b8f58b9d0285c4bc8b77d
JoaoGabrielPereiraLopes <gabriel.pitito123@gmail.com>
2026-03-02 23:34:37 -0300

    pequena alteração

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function CardInit(){
         document.getElementById('Carta').style.backgroundImage='url("https://www.bing.com/th/id/OIP.DRyy3X-N2MbXJfGW0M_4TAHaJ4?w=160&h=211&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2")'
         document.getElementById('Carta').style.height="90%"
         document.getElementById('Carta').style.width="90%"
-        document.getElementById('Carta').innerHTML="<H1>Legal</H1>"
+        document.getElementById('Carta').innerHTML="<H1>TEXTO TESTE PARA VER SE VOCÊ CONSEGUE LER</H1>"
         
     }, 2500);
     setTimeout(function() {
