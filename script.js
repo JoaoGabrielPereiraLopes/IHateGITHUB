4f42a8256a7fdfb85293056df6e16460f88c5266
Joâo Gabriel Pereira Lopes <gabriel.pitito123@gmail.com>
2026-03-02 23:39:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+async function CardInit(){
+    document.getElementById('Carta').style.animation='5000ms abrir'
+    setTimeout(function() {
+        document.getElementById('Carta').style.backgroundImage='url("https://www.bing.com/th/id/OIP.DRyy3X-N2MbXJfGW0M_4TAHaJ4?w=160&h=211&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2")'
+        document.getElementById('Carta').style.height="90%"
+        document.getElementById('Carta').style.width="90%"
+        document.getElementById('Carta').innerHTML="<H1>TEXTO TESTE PARA VER SE VOCÊ CONSEGUE LER</H1>"
+        
+    }, 2500);
+    setTimeout(function() {
+        document.getElementById('Carta').style.animation=''
+        
+    }, 5000);
+    console.log(0/0)
+}
